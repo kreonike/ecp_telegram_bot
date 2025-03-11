749d60482642b445ee44b9509fe6743c901118f9
kreon <kreonike@gmail.com>
2025-03-11 08:50:13 +0300

    11.03.2025 расширил прогресс загрузки, теперь он показывается и в чате средствами tqdm

--- api/search_time.py
+++ api/search_time.py
@@ -1,16 +1,17 @@
 import logging
 import requests
+from aiogram import Bot
+from tqdm import tqdm
 from api import authorization
 from config.config import API_ECP
-from tqdm import tqdm  # Импортируем tqdm для отображения прогресса
+import asyncio
 
 # Константы
 ALLOWED_TIMETABLE_TYPES = {'1', '4', '10', '11'}
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
-
-def fetch_available_times(med_staff_fact_id, beg_time, session):
+async def fetch_available_times(med_staff_fact_id, beg_time, session):
     url = f'{API_ECP}TimeTableGraf/TimeTableGrafFreeTime?MedStaffFact_id={med_staff_fact_id}&TimeTableGraf_begTime={beg_time}&sess_id={session}'
     try:
         response = requests.get(url)
@@ -20,8 +21,7 @@ def fetch_available_times(med_staff_fact_id, beg_time, session):
         logging.error(f'Ошибка при получении доступного времени: {e}')
         raise
 
-
-def fetch_timetable_details(time_table_graf_id, session):
+async def fetch_timetable_details(time_table_graf_id, session):
     url = f'{API_ECP}TimeTableGraf/TimeTableGrafById?TimeTableGraf_id={time_table_graf_id}&sess_id={session}'
     try:
         response = requests.get(url)
@@ -31,12 +31,10 @@ def fetch_timetable_details(time_table_graf_id, session):
         logging.error(f'Ошибка при получении деталей расписания: {e}')
         raise
 
-
 def filter_allowed_timetable_types(data):
     return [item for item in data if item.get('TimeTableType_id') in ALLOWED_TIMETABLE_TYPES]
 
-
-def search_time(med_staff_fact_id, data_date_dict):
+async def search_time(med_staff_fact_id, data_date_dict, bot: Bot, message):
     logging.info(f'Поиск доступного времени для MedStaffFact_id: {med_staff_fact_id}')
 
     # Авторизация
@@ -45,16 +43,37 @@ def search_time(med_staff_fact_id, data_date_dict):
     # Извлечение времени начала
     beg_time_list = [key['TimeTableGraf_begTime'] for key in data_date_dict.get('data', [])]
     logging.info(f'Список времени начала: {beg_time_list}')
+    logging.info(f"Количество элементов в beg_time_list: {len(beg_time_list)}")
 
     # Поиск доступного времени
     data_time_list = []
     total_tasks = len(beg_time_list)
-    for beg_time in tqdm(beg_time_list, desc="Поиск доступного времени", unit="запрос"):  # Добавляем прогресс бар
-        time_data = fetch_available_times(med_staff_fact_id, beg_time, session)
+
+    # Отправляем начальное сообщение
+    progress_message = await bot.send_message(
+        message.chat.id,
+        "Идёт поиск сводных дат для записи, это может занять много времени, пожалуйста ожидайте.. (0%)"
+    )
+
+    # Используем tqdm для отслеживания прогресса
+    for i, beg_time in enumerate(tqdm(beg_time_list, desc="Поиск доступного времени", unit="запрос")):
+        time_data = await fetch_available_times(med_staff_fact_id, beg_time, session)
         for item in time_data.get('data', []):
-            timetable_details = fetch_timetable_details(item['TimeTableGraf_id'], session)
+            timetable_details = await fetch_timetable_details(item['TimeTableGraf_id'], session)
             filtered_data = filter_allowed_timetable_types(timetable_details.get('data', []))
             data_time_list.extend(filtered_data)
+
+        # Вычисляем процент выполнения
+        progress_percent = int(((i + 1) / total_tasks) * 100)
+
+        # Обновляем сообщение на каждой итерации
+        logging.info(f"Обновление прогресса: {progress_percent}%")
+        await bot.edit_message_text(
+            chat_id=message.chat.id,
+            message_id=progress_message.message_id,
+            text=f"Идёт поиск сводных дат для записи, это может занять много времени, пожалуйста ожидайте.. ({progress_percent}%)"
+        )
+        await asyncio.sleep(1)  # Задержка 1 секунда для соответствия лимитам Telegram
 
     logging.info(f'Найдено доступных времен: {len(data_time_list)}')
     return data_time_list
--- handlers/doctor_handler.py
+++ handlers/doctor_handler.py
@@ -5,30 +5,18 @@ from aiogram.fsm.context import FSMContext
 from aiogram.types import KeyboardButton
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
-from api import search_date, search_time
+from api import search_date
+from api.search_time import search_time
 from handlers.return_to_main_menu_handler import return_to_main_menu
-# from handlers.spec_handler import get_spec
 from keyboards.client_kb import kb_client
-# from main import return_to_main_menu
 from states.states import ClientRequests
 
-# from config.config import global_spec_dict_final
-
-
 logger = logging.getLogger(__name__)
-
-# from utils.json_temp_data import load_global_spec_dict_final
-# print(f'doctor enter value', load_global_spec_dict_final())
-# spec_dict_final = load_global_spec_dict_final()
 
 async def get_doctor(message: types.Message, state: FSMContext):
     from utils.json_temp_data import load_global_spec_dict_final
     print(f'doctor enter value', load_global_spec_dict_final())
     spec_dict_final = load_global_spec_dict_final()
-
-    #global spec_dict_final
-    print('test test test')
-    print(f't2: {spec_dict_final}')
 
     mess = message.text
     print(f' message_handler() mess: {mess}')
@@ -36,66 +24,36 @@ async def get_doctor(message: types.Message, state: FSMContext):
     if mess == 'вернуться в меню':
         await state.set_state(ClientRequests.main_menu)
         await message.reply('выберите раздел', reply_markup=kb_client)
-        # spec_dict_final = {}
         await state.clear()
-
     else:
-        await message.answer(
-                               'Идёт поиск сводных дат для записи, это может занять много времени, пожалуйста ожидайте..')
-
-        #print(f' ?', MedStaffFact_id)
-        #global MedStaffFact_id
-
-        print(f' spec_dict_final в doctor_handler', spec_dict_final)
-        print(f' mess в doctor_handler', mess)
-
-
-        MedStaffFact_id = (spec_dict_final[mess])
-        print(f' MedStaffFact_id в doctor_handler', MedStaffFact_id)
-
+        MedStaffFact_id = spec_dict_final[mess]
         from utils.json_temp_data import save_global_medstafffact_id
         save_global_medstafffact_id(MedStaffFact_id)
-
-        await state.update_data(MedStaffFact_id=MedStaffFact_id)  ################
-
-        print(f' @@ MedStaffFact_id: {MedStaffFact_id}')
+        await state.update_data(MedStaffFact_id=MedStaffFact_id)
 
         """поиск даты"""
-        data_date_dict = {}
         data_date_dict = search_date.search_date(MedStaffFact_id)
         print(f' это дата лист из функции: {data_date_dict}')
 
-        data_time_final = search_time.search_time(MedStaffFact_id, data_date_dict)
+        # Передаем bot из глобального контекста
+        from main import bot  # Импортируем bot из основного файла
+        data_time_final = await search_time(MedStaffFact_id, data_date_dict, bot=bot, message=message)
         print(f' data_time_final = {data_time_final}')
 
         if not data_time_final:
-            # Если свободных дат нет
             await message.answer('На ближайшие 14 дней нет свободных дат к данному специалисту.')
             await return_to_main_menu(message, state)
         else:
-
             builder = ReplyKeyboardBuilder()
-
-            # Добавляем кнопку "вернуться в меню" наверху (на всю ширину ряда)
             builder.row(KeyboardButton(text="вернуться в меню"))
-
-            # Добавляем остальные кнопки из data_time_final
             for i in data_time_final:
                 builder.add(KeyboardButton(text=i['TimeTableGraf_begTime']))
+            builder.adjust(1, 2)
 
-            # Группируем все кнопки ниже "вернуться в меню" по 2 в строке
-            builder.adjust(1, 2)  # Первый ряд (1 кнопка), остальные по 2
-
-            markup = builder.as_markup(resize_keyboard=True)
-            keyboard = builder.as_markup(resize_keyboard=True)
-
-            # Отправляем сообщение с клавиатурой
             await message.answer(
                 'На ближайшие 14 дня есть следующие свободные даты:\n'
                 'Выберите желаемую дату приёма:',
                 reply_markup=builder.as_markup(resize_keyboard=True)
             )
             await state.set_state(ClientRequests.time)
-
-            # Логирование
             logger.info(f"MedStaffFact_id: {MedStaffFact_id}")

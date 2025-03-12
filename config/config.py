9e4b5c6a4b2815fb1b0963f203e24968126ca210
kreon <kreonike@gmail.com>
2025-03-12 11:20:04 +0300

    12.03.2025 забыл в комиты добавить .env

--- config/config.py
+++ config/config.py
@@ -1,13 +1,11 @@
 import os
 
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
-env_file = '.env.template'
-
-if not os.path.exists(env_file):
-    exit(f"Переменные окружения не загружены т.к отсутствует файл {env_file}")
+if not find_dotenv():
+    exit("Переменные окружения не загружены т.к отсутствует файл .env")
 else:
-    load_dotenv(dotenv_path=env_file)
+    load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 LOGIN_ECP = os.getenv('LOGIN_ECP')
